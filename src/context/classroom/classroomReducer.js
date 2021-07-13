a667a34bb59f39a380fac05fa50a2903cf5740d0
Paul Walter <paulwalter@Pauls-Mac-mini.local>
2021-07-13 10:32:32 +0100

    state and reducer complete for local app, not tested

--- src/context/classroom/classroomReducer.js
+++ src/context/classroom/classroomReducer.js
@@ -1,41 +1,152 @@
 /* eslint-disable import/no-anonymous-default-export */
 import {
-    ADD_CLASS_REMINDER,
-    DECREMENT_CURRENT_REMINDER,
-    DELETE_CURRENT_REMINDER,
-    INCREMENT_CURRENT_REMINDER,
-  } from "../types";
-
+  ADD_CLASS_REMINDER,
+  CLEAR_STUDENT_REMINDER,
+  CYCLE_STUDENT_FLAG,
+  DECREMENT_CURRENT_PROGRESS_SCALE,
+  DECREMENT_CURRENT_REMINDER,
+  DECREMENT_STUDENT_PROGRESS,
+  DELETE_CURRENT_REMINDER,
+  EDIT_CURRENT_PROGRESS_SCALE,
+  EDIT_STUDENT_REMINDER,
+  INCREMENT_CURRENT_PROGRESS_SCALE,
+  INCREMENT_CURRENT_REMINDER,
+  INCREMENT_STUDENT_PROGRESS,
+  SET_CURRENT_DESK,
+  STUDENT_FLAG_COUNT,
+  SWAP_STUDENTS,
+} from "../types";
 
 export default (state, action) => {
-    switch (action.type) {
-      case ADD_CLASS_REMINDER:
-          return {
-              ...state,
-              class_reminders: [...state.class_reminders, action.payload]
-          };
-        case DELETE_CURRENT_REMINDER:
-            let modified_reminders = [...state.class_reminders];
-            modified_reminders.splice(state.current_reminder, 1);
-            let new_current_reminder = state.current_reminder;
-            if (new_current_reminder >= modified_reminders.length && new_current_reminder > 0) new_current_reminder -= 1;
-            return {
-              ...state,  
-              class_reminders: modified_reminders,
-              current_reminder: new_current_reminder
-            };
-        case DECREMENT_CURRENT_REMINDER:
-            return {
-                ...state,
-                current_reminder: (state.current_reminder > 0 ? state.current_reminder - 1 : 0),
-            };
-        case INCREMENT_CURRENT_REMINDER:
-            return {
-                ...state,
-                current_reminder: (state.current_reminder + 1 < state.class_reminders.length ? state.current_reminder + 1 : state.current_reminder),
-            };
-    
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case ADD_CLASS_REMINDER:
+      return {
+        ...state,
+        class_reminders: [...state.class_reminders, action.payload],
+      };
+    case DELETE_CURRENT_REMINDER:
+      let modified_reminders = [...state.class_reminders];
+      modified_reminders.splice(state.current_reminder, 1);
+      let new_current_reminder = state.current_reminder;
+      if (
+        new_current_reminder >= modified_reminders.length &&
+        new_current_reminder > 0
+      )
+        new_current_reminder -= 1;
+      return {
+        ...state,
+        class_reminders: modified_reminders,
+        current_reminder: new_current_reminder,
+      };
+    case DECREMENT_CURRENT_REMINDER:
+      return {
+        ...state,
+        current_reminder:
+          state.current_reminder > 0 ? state.current_reminder - 1 : 0,
+      };
+    case INCREMENT_CURRENT_REMINDER:
+      return {
+        ...state,
+        current_reminder:
+          state.current_reminder + 1 < state.class_reminders.length
+            ? state.current_reminder + 1
+            : state.current_reminder,
+      };
+    case DECREMENT_CURRENT_PROGRESS_SCALE:
+      return {
+        ...state,
+        current_scale: state.current_scale > 0 ? state.current_scale - 1 : 0,
+      };
+    case INCREMENT_CURRENT_PROGRESS_SCALE:
+      return {
+        ...state,
+        current_scale:
+          state.current_scale + 1 < state.progress_scales.length
+            ? state.current_scale + 1
+            : state.current_scale,
+      };
+    case INCREMENT_STUDENT_PROGRESS:
+      return {
+        ...state,
+        students: state.students.map((student) =>
+          student.desk_id === action.payload
+            ? {
+                ...student,
+                progress: student.progress.map((item, index) =>
+                  index === state.current_scale &&
+                  item + 1 < state.progress_scales[state.current_scale].length
+                    ? item + 1
+                    : item
+                ),
+              }
+            : student
+        ),
+      };
+    case DECREMENT_STUDENT_PROGRESS:
+      return {
+        ...state,
+        students: state.students.map((student) =>
+          student.desk_id === action.payload
+            ? {
+                ...student,
+                progress: student.progress.map((item, index) =>
+                  index === state.current_scale && item > 0 ? item - 1 : item
+                ),
+              }
+            : student
+        ),
+      };
+    case EDIT_STUDENT_REMINDER:
+      return {
+        ...state,
+        students: state.students.map((student) =>
+          student.desk_id === action.payload.desk_id
+            ? { ...student, reminder: action.payload.text }
+            : student
+        ),
+      };
+    case CLEAR_STUDENT_REMINDER:
+      return {
+        ...state,
+        students: state.students.map((student) =>
+          student.desk_id === action.payload
+            ? { ...student, reminder: null }
+            : student
+        ),
+      };
+    case SET_CURRENT_DESK:
+      return {
+        ...state,
+        current_desk: action.payload,
+      };
+    case CYCLE_STUDENT_FLAG:
+      return {
+        ...state,
+        students: state.students.map((student) =>
+          student.desk_id === action.payload
+            ? { ...student, flag: (student.flag + 1) % STUDENT_FLAG_COUNT }
+            : student
+        ),
+      };
+    case SWAP_STUDENTS:
+      return {
+        ...state,
+        students: state.students.map((student) =>
+          student.desk_id === action.payload.first_desk_id
+            ? { ...student, desk_id: action.payload.second_desk_id }
+            : student.desk_id === action.payload.second_desk_id
+            ? { ...student, desk_id: action.payload.first_desk_id }
+            : student
+        ),
+      };
+    case EDIT_CURRENT_PROGRESS_SCALE:
+      return {
+        ...state,
+        progress_scales: state.progress_scales.map((scale, index) =>
+          index === state.current_scale ? action.payload : scale
+        ),
+      };
+    default:
+      return state;
+  }
+};
--- src/context/classroom/classroomState.js
+++ src/context/classroom/classroomState.js
@@ -4,40 +4,119 @@ import ClassroomContext from "./classroomContext";
 // import axios from "axios";
 import {
   ADD_CLASS_REMINDER,
+  CLEAR_STUDENT_REMINDER,
+  DECREMENT_CURRENT_PROGRESS_SCALE,
   DECREMENT_CURRENT_REMINDER,
+  DECREMENT_STUDENT_PROGRESS,
   DELETE_CURRENT_REMINDER,
-  INCREMENT_CURRENT_REMINDER
+  EDIT_CURRENT_PROGRESS_SCALE,
+  EDIT_STUDENT_REMINDER,
+  INCREMENT_CURRENT_PROGRESS_SCALE,
+  INCREMENT_CURRENT_REMINDER,
+  INCREMENT_STUDENT_PROGRESS,
+  SET_CURRENT_DESK,
+  SWAP_STUDENTS,
 } from "../types";
 
 const ClassroomState = (props) => {
   const initialState = {
-    students: [],   //Each student has id, name, desk_id, reminder?, progress_points [], flag
-    desks: [],      //Each desk had id, posx, posy 
+    students: [
+      {
+        id: 1,
+        name: "Harry",
+        desk_id: 3,
+        reminder: null,
+        progress: [0, 0, 0, 0, 0],
+        flag: 0,
+      },
+      {
+        id: 2,
+        name: "Hermione",
+        desk_id: 1,
+        reminder: null,
+        progress: [1, 3, 0, 0, 0],
+        flag: 1,
+      },
+      {
+        id: 3,
+        name: "Ron",
+        desk_id: 2,
+        reminder: "Give back Prof Snape's book",
+        progress: [0, 1, 4, 0, 0],
+        flag: 0,
+      },
+    ], //Each student has id, name, desk_id, reminder?, progress_points [], flag
+    desks: [], //Each desk had id, posx, posy
     desk_size_x: 10,
     desk_size_y: 10,
-    progress_scales: [],
+    progress_scales: [
+      ["", "tick", "cross"],
+      ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+      ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    ],
     class_reminders: ["alpha", "beta", "gamma", "delta"],
-    current_reminder: 1,
-    current_scale: 0,
-    current_desk: 0
+    current_reminder: 1, // index in class_reminders array
+    current_scale: 0, // index in progress_scales array
+    current_desk: 0, // id attribute of the desk
   };
   const [state, dispatch] = useReducer(classroomReducer, initialState);
 
   const addClassReminder = (reminder_text) => {
-      dispatch({type: ADD_CLASS_REMINDER, payload: reminder_text})
-  }
+    dispatch({ type: ADD_CLASS_REMINDER, payload: reminder_text });
+  };
 
   const deleteCurrentClassReminder = () => {
-      dispatch({type: DELETE_CURRENT_REMINDER, payload: null})
-  }
+    dispatch({ type: DELETE_CURRENT_REMINDER, payload: null });
+  };
 
   const incrementCurrentReminder = () => {
-    dispatch({type: INCREMENT_CURRENT_REMINDER, payload: null})
-  }
+    dispatch({ type: INCREMENT_CURRENT_REMINDER, payload: null });
+  };
 
   const decrementCurrentReminder = () => {
-    dispatch({type: DECREMENT_CURRENT_REMINDER, payload: null})
-  }
+    dispatch({ type: DECREMENT_CURRENT_REMINDER, payload: null });
+  };
+
+  const decrementCurrentProgressScale = () => {
+    dispatch({ type: DECREMENT_CURRENT_PROGRESS_SCALE, payload: null });
+  };
+
+  const incrementCurrentProgressScale = () => {
+    dispatch({ type: INCREMENT_CURRENT_PROGRESS_SCALE, payload: null });
+  };
+
+  const incrementStudentProgress = (desk_id) => {
+    dispatch({ type: INCREMENT_STUDENT_PROGRESS, payload: desk_id });
+  };
+
+  const decrementStudentProgress = (desk_id) => {
+    dispatch({ type: DECREMENT_STUDENT_PROGRESS, payload: desk_id });
+  };
+
+  const editStudentReminder = (desk_id, text) => {
+    dispatch({ type: EDIT_STUDENT_REMINDER, payload: { desk_id, text } });
+  };
+
+  const clearStudentReminder = (desk_id) => {
+    dispatch({ type: CLEAR_STUDENT_REMINDER, payload: desk_id });
+  };
+
+  const setCurrentDesk = (desk_id) => {
+    dispatch({ type: SET_CURRENT_DESK, payload: desk_id });
+  };
+
+  const swapStudents = (first_desk_id, second_desk_id) => {
+    dispatch({
+      type: SWAP_STUDENTS,
+      payload: { first_desk_id, second_desk_id },
+    });
+  };
+
+  const editCurrentProgressScale = (new_values) => {
+    dispatch({ type: EDIT_CURRENT_PROGRESS_SCALE, payload: new_values });
+  };
 
   return (
     <ClassroomContext.Provider
@@ -55,6 +134,15 @@ const ClassroomState = (props) => {
         deleteCurrentClassReminder,
         incrementCurrentReminder,
         decrementCurrentReminder,
+        decrementCurrentProgressScale,
+        incrementCurrentProgressScale,
+        decrementStudentProgress,
+        incrementStudentProgress,
+        editStudentReminder,
+        clearStudentReminder,
+        setCurrentDesk,
+        swapStudents,
+        editCurrentProgressScale,
       }}
     >
       {props.children}
