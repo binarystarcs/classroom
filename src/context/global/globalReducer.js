95cb4c7edba898aca7fc94e2c07745b5c9aa267c
Paul Walter <paulwalter@Pauls-Mac-mini.local>
2021-07-19 10:46:47 +0100

    first draft of global reducer

--- src/context/global/globalReducer.js
+++ src/context/global/globalReducer.js
@@ -1,12 +1,205 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { CLEAR_CURRENT_CLASS } from "../types";
+import {
+  ADD_ROOM,
+  ADD_SET,
+  CLEAR_CURRENT_ROOM,
+  CLEAR_CURRENT_SET,
+  EDIT_CURRENT_ROOM,
+  EDIT_CURRENT_SEATING,
+  EDIT_CURRENT_SET,
+  SET_CURRENT_ROOM,
+  SET_CURRENT_SET,
+} from "../types";
+
+const updateSeatingWithClass = (seating_list, set_list, room_desk_count) => {
+  let expanded_seating_list = seating_list;
+  for (let i = seating_list.length; i < room_desk_count; i += 1)
+    expanded_seating_list.push(null);
+  const eliminated_list = expanded_seating_list
+    .slice(0, room_desk_count)
+    .map((value) =>
+      set_list.some((student) => student.id === value) ? value : null
+    );
+  const unseated_students = set_list
+    .filter((student) => seating_list.some((value) => value === student.id))
+    .map((student) => student.id);
+  return eliminated_list.map((value) =>
+    value === null
+      ? unseated_students.length
+        ? unseated_students.shift()
+        : null
+      : value
+  );
+};
 
 export default (state, action) => {
   switch (action.type) {
-    case CLEAR_CURRENT_CLASS:
+    case CLEAR_CURRENT_SET:
       return {
         ...state,
-        current_class: null,
+        current_set: null,
+        current_seating: null,
+        error: null,
+      };
+    case CLEAR_CURRENT_ROOM:
+      return {
+        ...state,
+        current_room: null,
+        current_seating: null,
+        error: null,
+      };
+    case SET_CURRENT_SET:
+      if (!state.sets.some((setObj) => setObj.name === action.payload))
+        return {
+          ...state,
+          error: "Set does not exist",
+          current_set: null,
+          current_seating: null,
+        };
+      return {
+        ...state,
+        current_set: action.payload,
+        current_seating:
+          state.seatings.find(
+            (seating) =>
+              seating.room_name === state.current_room &&
+              seating.class_name === state.current_set
+          ) || null,
+      };
+    case SET_CURRENT_ROOM:
+      if (!state.rooms.some((setObj) => setObj.name === action.payload))
+        return {
+          ...state,
+          error: "Room does not exist",
+          current_set: null,
+          current_seating: null,
+        };
+      return {
+        ...state,
+        current_room: action.payload,
+        current_seating:
+          state.seatings.find(
+            (seating) =>
+              seating.room_name === state.current_room &&
+              seating.class_name === state.current_set
+          ) || null,
+      };
+    case ADD_SET:
+      if (state.sets.some((setObj) => setObj.name === action.payload))
+        return {
+          ...state,
+          error: "Set already exists",
+        };
+      return {
+        ...state,
+        sets: [
+          ...state.sets,
+          { name: action.payload, reminders: [], students: [] },
+        ],
+        seatings: [
+          ...state.seatings,
+          ...state.rooms.map((room) => ({
+            room_name: room.name,
+            class_name: action.payload,
+            students: new Array(room.desks.length).fill(null),
+          })),
+        ],
+        error: null,
+      };
+    case ADD_ROOM:
+      if (state.rooms.some((setObj) => setObj.name === action.payload))
+        return {
+          ...state,
+          error: "Room already exists",
+        };
+      return {
+        ...state,
+        rooms: [
+          ...state.rooms,
+          { name: action.payload, desk_width: 16, desk_height: 12, desks: [] },
+        ],
+        seatings: [
+          ...state.seatings,
+          ...state.sets.map((setObj) => ({
+            room_name: action.payload,
+            class_name: setObj.name,
+            students: [],
+          })),
+        ],
+        error: null,
+      };
+    case EDIT_CURRENT_SET:
+      if (!state.current_set) return { ...state, error: "No current set" };
+      return {
+        ...state,
+        error: null,
+        sets: state.sets.map((setObj) =>
+          setObj.name === state.current_set
+            ? { ...setObj, students: action.payload }
+            : setObj
+        ),
+        seatings: state.seatings.map((seating) =>
+          seating.class_name === state.current_set
+            ? updateSeatingWithClass(
+                seating.students,
+                action.payload,
+                seating.students.length
+              )
+            : seating
+        ),
+      };
+    case EDIT_CURRENT_ROOM:
+      if (!state.current_room) return { ...state, error: "No current room" };
+      return {
+        ...state,
+        error: null,
+        rooms: state.rooms.map((room) =>
+          room.name === state.current_room
+            ? {
+                ...room,
+                desk_width: action.payload.desk_width,
+                desk_height: action.payload.desk_height,
+                desks: action.payload.desks,
+              } // Consider validating action.payload.desks
+            : room
+        ),
+        seatings: state.seatings.map((seating) =>
+          seating.room_name === state.current_room
+            ? updateSeatingWithClass(
+                seating.students,
+                state.sets.find((setObj) => setObj.name === seating.class_name)
+                  .students,
+                action.payload.desks.length
+              )
+            : seating
+        ),
+      };
+    case EDIT_CURRENT_SEATING:
+      if (!state.current_seating)
+        return { ...state, error: "No current seating" };
+      if (action.payload.length !== state.current_seating.students.length)
+        return { ...state, error: "Seating arrangement length incorrect." };
+      if (
+        [...action.payload].sort() !==
+        [...state.current_seating.students].sort()
+      )
+        return { ...state, error: "Incorrect student IDs" };
+      return {
+        ...state,
+        seatings: state.seatings.map((seating) =>
+          seating.room_name === state.current_seating.room_name &&
+          seating.class_name === state.current_seating.class_name
+            ? {
+                ...seating,
+                students: action.payload,
+              }
+            : seating
+        ),
+        current_seating: {
+          ...state.current_seating,
+          students: action.payload,
+        },
+        error: null,
       };
     default:
       return state;
--- src/context/global/globalState.js
+++ src/context/global/globalState.js
@@ -2,23 +2,160 @@ import React, { useReducer } from "react";
 import globalReducer from "./globalReducer";
 import GlobalContext from "./globalContext";
 // import axios from "axios";
-import { CLEAR_CURRENT_CLASS } from "../types";
+import {
+  ADD_ROOM,
+  ADD_SET,
+  CLEAR_CURRENT_ROOM,
+  CLEAR_CURRENT_SET,
+  EDIT_CURRENT_ROOM,
+  EDIT_CURRENT_SEATING,
+  EDIT_CURRENT_SET,
+  SET_CURRENT_ROOM,
+  SET_CURRENT_SET,
+} from "../types";
 
 const GlobalState = (props) => {
   const initialState = {
-    current_class: null,
+    rooms: [
+      {
+        name: "Room A",
+        desk_width: 16,
+        desk_height: 12,
+        desks: [
+          { id: 0, x: 20, y: 40 },
+          { id: 1, x: 44, y: 40 },
+          { id: 2, x: 68, y: 40 },
+          { id: 3, x: 32, y: 70 },
+          { id: 4, x: 56, y: 70 },
+        ],
+      },
+      {
+        name: "Room B",
+        desk_width: 14,
+        desk_height: 10,
+        desks: [
+          { id: 0, x: 10, y: 33 },
+          { id: 1, x: 30, y: 33 },
+          { id: 2, x: 50, y: 33 },
+          { id: 3, x: 70, y: 33 },
+        ],
+      },
+    ],
+    current_room: null,
+    sets: [
+      {
+        name: "Potions",
+        reminders: ["Give back books", "Reschedule lesson"],
+        students: [
+          {
+            id: 1,
+            name: "Harry",
+            reminder: null,
+          },
+          {
+            id: 2,
+            name: "Ron",
+            reminder: "Half-blood prince book",
+          },
+          {
+            id: 3,
+            name: "Hermione",
+            reminder: null,
+          },
+        ],
+      },
+      {
+        name: "Dark Arts Defence",
+        reminders: null,
+        students: [
+          {
+            id: 4,
+            name: "Harry",
+            reminder: null,
+          },
+          {
+            id: 2,
+            name: "Draco",
+            reminder: "Call dad",
+          },
+          {
+            id: 3,
+            name: "Neville",
+            reminder: null,
+          },
+        ],
+      },
+    ],
+    current_set: null,
+    seatings: [
+      {
+        room_name: "Room A",
+        class_name: "Potions",
+        students: [3, 1, null, 2, null],
+      },
+      {
+        room_name: "Room B",
+        class_name: "Dark Arts Defence",
+        students: [2, null, 4, 3],
+      },
+    ],
   };
 
   const [state, dispatch] = useReducer(globalReducer, initialState);
 
-  const clearCurrentClass = () => {
-    dispatch({ type: CLEAR_CURRENT_CLASS, payload: null });
+  const clearCurrentSet = () => {
+    dispatch({ type: CLEAR_CURRENT_SET, payload: null });
   };
+
+  const clearCurrentRoom = () => {
+    dispatch({ type: CLEAR_CURRENT_ROOM, payload: null });
+  };
+
+  const addRoom = (room_name) => {
+    dispatch({ type: ADD_ROOM, payload: room_name });
+  };
+
+  const addSet = (set_name) => {
+    dispatch({ type: ADD_SET, payload: set_name });
+  };
+
+  const setCurrentSet = (set_name) => {
+    dispatch({ type: SET_CURRENT_SET, payload: set_name });
+  };
+
+  const setCurrentRoom = (room_name) => {
+    dispatch({ type: SET_CURRENT_ROOM, payload: room_name });
+  };
+
+  const updateCurrentRoom = (room_data) => {
+    dispatch({ type: EDIT_CURRENT_ROOM, payload: room_data });
+  };
+
+  const updateCurrentSet = (set_data) => {
+    dispatch({ type: EDIT_CURRENT_SET, payload: set_data });
+  };
+
+  const updateCurrentSeating = (seating_data) => {
+    dispatch({ type: EDIT_CURRENT_SEATING, payload: seating_data });
+  };
+
   return (
     <GlobalContext.Provider
       value={{
-        current_class: state.current_class,
-        clearCurrentClass,
+        rooms: state.rooms,
+        sets: state.sets,
+        seatings: state.seatings,
+        current_room: state.current_room,
+        current_set: state.current_set,
+        clearCurrentSet,
+        clearCurrentRoom,
+        addSet,
+        addRoom,
+        setCurrentSet,
+        setCurrentRoom,
+        updateCurrentRoom,
+        updateCurrentSeating,
+        updateCurrentSet,
       }}
     >
       {props.children}
